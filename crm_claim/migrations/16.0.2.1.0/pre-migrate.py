bfa98b5e71693798d185e3be9ec31c779e3d8d04
Michael Dahl <michael@dahl.software>
2024-01-11 11:36:43 +0100

    Remove views in migration

--- crm_claim/migrations/16.0.2.1.0/pre-migrate.py
+++ crm_claim/migrations/16.0.2.1.0/pre-migrate.py
@@ -2,6 +2,42 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    cr.execute(
+        """
+            SELECT res_id
+              FROM ir_model_data d
+             WHERE NOT EXISTS (SELECT 1
+                                 FROM ir_model_data
+                                WHERE id != d.id
+                                  AND res_id = d.res_id
+                                  AND model = d.model
+                                  AND module != d.module)
+               AND module = 'crm_claim'
+               AND model = 'ir.ui.view'
+          ORDER BY id DESC
+            """
+    )
+    for view_id in cr.fetchall():
+        util.remove_view(cr, view_id=view_id)
+
+    cr.execute(
+        """
+            SELECT res_id
+              FROM ir_model_data d
+             WHERE NOT EXISTS (SELECT 1
+                                 FROM ir_model_data
+                                WHERE id != d.id
+                                  AND res_id = d.res_id
+                                  AND model = d.model
+                                  AND module != d.module)
+               AND module = 'crm_claim'
+               AND model = 'ir.ui.menu'
+          ORDER BY id DESC
+    """
+    )
+    menu_ids = cr.fetchall()
+    util.remove_menus(cr, menu_ids)
+
     # crm_claim to helpdesk_ticket
     util.create_column(cr, "helpdesk_stage", "_stage_id", "int4")
 
